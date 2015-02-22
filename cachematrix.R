6383313e5d0ab12f3a62e276b1bd5ebada015107
paperless247 <paperless247@gmail.com>
2015-02-22 08:02:44 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## Below are two functions that are used to create
+## a special object that stores a matrix and cache's its inverse 
 
-## Write a short comment describing this function
+
+## makeCacheMatrix will create a special "vector",
+## which is really a list containing a function to
+##    set the value of the matrix
+##    get the value of the matrix
+##    set the value of the inverse matrix
+##    get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        iM <- NULL                # inverse matrix of x
+        set <- function(y) {      # set values of matrix
+                x <<- y
+                iM <<- NULL
+        }
+        get <- function() x       # get values of matrix
+        setsolve <- function(solve) iM <<- solve # call solve to set value of iM
+        getsolve <- function() iM # get value of iM
+        list(set = set, get = get,
+                setsolve = setsolve,
+                getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+
+
+## The following function will check if the inverse matrix has already been calculated
+## If yes, return the catched inverse matrix
+## If no, calculate the inverse matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        ## assume that 'x' is always invertible
+        iM <- x$getsolve()
+
+        ## checks to see if the iM has already been calculated
+        if(!is.null(iM)) {
+        message("getting cached data")
+        return(iM)              # return the catched values, exit the function
+        }
+
+        ## get data and  calculate the iM
+        data <- x$get()
+        iM <- solve(data, ...)  # calculate the inverse matrix
+        x$setsolve(iM)
+        iM                      # return the calculated iM
 }
